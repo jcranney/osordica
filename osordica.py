4073189a1956e1af90a8e037f8299fca49b4fc4c
jcranney <c3162562@uon.edu.au>
2017-01-07 23:53:18 +1100

    Create osordica.py
    Initial commit

---
+++ osordica.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+from scipy.io import wavfile
+import argparse
+import numpy as np
+import pygame
+import sys
+import warnings
+
+#need to map raspberrypi io's
+
+#need to load audio sample file
+
+#need to generate clean sine waves of required frequencies to see if it sounds better
+
+#need to stretch and attenuate/amplify scaled sound
+
+#need to do lots of other stuff, if you think of anything just add it
