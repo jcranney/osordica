44fdbf7ca1884d5a0650b90b38e83acc97ea5b23
Jesse Cranney <c3162562@uon.edu.au>
2017-01-08 21:40:32 +1100

    Added the pin map and the class for buttons

--- osordica.py
+++ osordica.py
@@ -6,6 +6,41 @@ import numpy as np
 import pygame
 import sys
 import warnings
+#import RPi.GPIO as GPIO
+
+# need 10 digitals to be used as keys:
+# L0 is left pinky, R4 is right thumb etc
+#
+#   Finger |  Pin  | GPIOnum
+#     L0   |   11  |   17
+#     L1   |   13  |   27
+#     L2   |   15  |   22
+#     L3   |   12  |   18 
+#     L4   |   16  |   23
+#     R0   |   29  |    5
+#     R1   |   31  |    6
+#     R2   |   33  |   13 
+#     R3   |   32  |   12 
+#     R4   |   37  |   26 
+#
+# 1 digital to be used as strum:
+#
+#	Strum  |   7   |    4  
+#
+
+class Button:
+	"""Button variable class for keyboard buttons"""
+	pressed = 0
+	def __init__(self,finger,pin,GPIOnum):
+		self.finger = finger
+		self.pin = pin
+		self.GPIOnum = GPIOnum
+
+pins = [11, 13 ,15, 12, 16, 29, 31, 33, 32, 37]
+GPIOnums = [17, 27, 22, 18, 23, 5, 6, 13, 12, 26]
+finger_names = ["L0", "L1", "L2", "L3", "L4", "R0", "R1", "R2", "R3", "R4"]
+fingers = [Button(finger_names[x],pins[x],GPIOnums[x]) for x in range(10)]
+
 
 #need to map raspberrypi io's
 
