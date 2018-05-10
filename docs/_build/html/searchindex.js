f84203d5b566644555862c18b61a4a67544ddbc2
Jesse Cranney <jcranney@dhcp-10-222-67-207.newcastle.edu.au>
2018-05-10 13:12:16 +1000

    added initial docs

---
+++ docs/_build/html/searchindex.js
@@ -0,0 +1,1 @@
+Search.setIndex({docnames:["contact","index"],envversion:51,filenames:["contact.rst","index.rst"],objects:{},objnames:{},objtypes:{},terms:{contact:1,content:1,faxo:0,index:1,modul:1,page:1,search:1},titles:["Contact","Welcome to osordica&#8217;s documentation!"],titleterms:{contact:0,document:1,indic:1,osordica:1,tabl:1,welcom:1}})
